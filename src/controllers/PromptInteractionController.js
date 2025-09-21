96bca0d5a83129be2f1f8376f37f7f7dfcc01b6a
BijonTalukder <bijontalukder1247@gmail.com>
2025-09-21 16:35:14 +0600

    updated

--- src/controllers/PromptInteractionController.js
+++ src/controllers/PromptInteractionController.js
@@ -4,18 +4,20 @@ class PromptInteractionController {
   }
 
   // Record a new interaction
-  async recordInteraction(req, res, next) {
-    try {
-      const { promptId, type } = req.body;
-      if (!promptId || !type)
-        return res.status(400).json({ success: false, message: "Missing promptId or type" });
+ async recordInteraction(req, res, next) {
+  try {
+    const { promptId, type } = req.body;
+    if (!promptId || !type)
+      return res.status(400).json({ success: false, message: "Missing promptId or type" });
 
-      const result = await this.interactionService.recordInteraction({ promptId, type });
-      res.status(201).json({ success: true, data: result });
-    } catch (error) {
-      next(error);
-    }
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+    const result = await this.interactionService.recordInteraction({ promptId, type, ip });
+    res.status(201).json({ success: true, data: result });
+  } catch (error) {
+    next(error);
   }
+}
 
   // Get interactions by prompt
   async getInteractions(req, res, next) {
