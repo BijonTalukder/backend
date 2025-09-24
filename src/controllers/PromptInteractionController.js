880ceb8f159239a63d04127cd1a4d7150f8c5e14
BijonTalukder <bijontalukder1247@gmail.com>
2025-09-24 11:35:31 +0600

    ip trace

--- src/controllers/PromptInteractionController.js
+++ src/controllers/PromptInteractionController.js
@@ -1,23 +1,26 @@
+const { getClientIp } = require("../utility/getClientIp");
+
 class PromptInteractionController {
   constructor(interactionService) {
     this.interactionService = interactionService;
   }
 
   // Record a new interaction
- async recordInteraction(req, res, next) {
-  try {
-    const { promptId, type } = req.body;
-    if (!promptId || !type)
-      return res.status(400).json({ success: false, message: "Missing promptId or type" });
+  async recordInteraction(req, res, next) {
+    try {
+      const { promptId, type } = req.body;
+      if (!promptId || !type)
+        return res.status(400).json({ success: false, message: "Missing promptId or type" });
 
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+      const ip = getClientIp(req);
 
-    const result = await this.interactionService.recordInteraction({ promptId, type, ip });
-    res.status(201).json({ success: true, data: result });
-  } catch (error) {
-    next(error);
+      const normalizedIp = ip?.replace(/^::ffff:/, "");
+      const result = await this.interactionService.recordInteraction({ promptId, type, ip: normalizedIp });
+      res.status(201).json({ success: true, data: result });
+    } catch (error) {
+      next(error);
+    }
   }
-}
 
   // Get interactions by prompt
   async getInteractions(req, res, next) {
