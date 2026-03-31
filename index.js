39c7c3d9dd3519d357b055db99016b0d66b1f2a5
Bijon Talukder <81519797+BijonTalukder@users.noreply.github.com>
2026-03-31 15:28:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors({
 // Middleware
 app.use(helmet());
 
-app.use(cors());
+//app.use(cors());
 // {
 //   origin: function (origin, callback) {
 //     if (!origin || allowedOrigins.includes(origin)) {
