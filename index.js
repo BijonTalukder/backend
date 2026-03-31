18058f3e0d5fe2b80cbd01532b43c365bf391ecb
Bijon Talukder <81519797+BijonTalukder@users.noreply.github.com>
2026-03-31 15:33:51 +0600

    Refactor CORS middleware configuration
    Comment out specific CORS configuration and enable default CORS.

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const swaggerUi = require('swagger-ui-express');
 const port = process.env.PORT || 5000;
 const prisma = new PrismaClient();
 const RSSParser = require('rss-parser');
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'https://gemini-apps.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: [
+//     'http://localhost:3000',
+//     'http://localhost:5173',
+//     'https://gemini-apps.vercel.app'
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   credentials: true,
+// }));
 // Middleware
 app.use(helmet());
 
-//app.use(cors());
+app.use(cors());
 // {
 //   origin: function (origin, callback) {
 //     if (!origin || allowedOrigins.includes(origin)) {
